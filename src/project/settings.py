7608fda8736e92785c421fd9238b268d2b5fb0dd
Sérgio Dantas <sergiodnts828@gmail.com>
2024-01-10 12:20:17 -0300

    chore: change default auto field

--- src/project/settings.py
+++ src/project/settings.py
@@ -140,4 +140,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
