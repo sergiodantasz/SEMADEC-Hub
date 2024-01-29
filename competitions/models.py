dbff262719c95c2b8b681c3246d9b0fb571fd70a
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-29 13:56:40 -0300

    feat: start building Competition model from `competitions` app

--- competitions/models.py
+++ competitions/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
+from editions.models import Edition
+
 
 class Competition(models.Model):
-    ...
+    edition = models.ForeignKey(
+        Edition,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        db_column='edition_year',
+    )
+    test_or_sport = models.ForeignKey(
+        TestOrSport,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        db_column='test_or_sport_id',
+    )
 
 
 class Test(models.Model):
