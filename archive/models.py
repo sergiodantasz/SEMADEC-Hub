9d7d7d1a1da8272b2e13206ac85bafb2e05b3418
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-29 13:14:55 -0300

    feat: start building Collection model from `archive` app

--- archive/models.py
+++ archive/models.py
@@ -1,8 +1,40 @@
 from django.db import models
 
+from users.models import Administrator
+
 
 class Collection(models.Model):
-    ...
+    administrator = models.ForeignKey(
+        Administrator,
+        on_delete=models.SET_NULL,
+        blank=False,
+        db_column='administrator_id',
+    )
+    title = models.CharField(
+        unique=True,
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    cover = models.ImageField(
+        upload_to='',  # Change it later
+        null=False,
+        blank=True,  # Can it be blank?
+    )
+    slug = models.CharField(
+        unique=True,
+        max_length=225,
+        null=False,
+        blank=False,
+    )
+    creation_date = models.DateField(
+        null=False,
+        blank=True,  # Can it be blank?
+    )
+    update_date = models.DateField(
+        null=False,
+        blank=True,  # Can it be blank?
+    )
 
 
 class CollectionTag(models.Model):
