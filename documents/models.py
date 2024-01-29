dc2caf08f8d99bfebc06e17337de9bdd3577d594
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-29 13:45:56 -0300

    feat: start building Document model from `documents` app

--- documents/models.py
+++ documents/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 
+from users.models import Administrator
+
 
 class Document(models.Model):
-    ...
-
-
-class DocumentTag(models.Model):
-    ...
+    administrator = models.ForeignKey(
+        Administrator,
+        on_delete=models.SET_NULL,
+        blank=False,
+        db_column='administrator_id',
+    )
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    name = ...
+    slug = models.SlugField(
+        unique=True,
+        max_length=225,
+        null=False,
+        blank=False,
+    )
+    creation_date = models.DateField(
+        null=False,
+        blank=True,  # Can it be blank?
+    )
+    update_date = models.DateField(
+        null=False,
+        blank=True,  # Can it be blank?
+    )
+    # Add ManyToManyField into Tag model
