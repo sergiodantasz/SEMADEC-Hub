2562ed82e1cc7a9c566b172ea9e0f3530d7d49f2
Sérgio Dantas <sergiodnts828@gmail.com>
2024-01-31 13:14:36 -0300

    feat: create Tag model from `home` app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    slug = models.CharField(
+        max_length=75,
+        unique=True,
+        null=False,
+        blank=False,
+    )
