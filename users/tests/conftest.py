fbb726b45d33a0711dd927ca017b5c61b546ab01
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-31 22:07:40 -0300

    refactor: remove useless `user_fixture_data` fixture

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -20,23 +20,6 @@ def campus_fixture():
     return CampusFactory()
 
 
-@pytest.fixture
-def user_fixture_data(campus_fixture, course_fixture):
-    return {
-        'registration': fake.random_number(digits=14),
-        'campus': campus_fixture,
-        'course': ...,
-        'full_name': ...,
-        'first_name': ...,
-        'last_name': ...,
-        'cpf': ...,
-        'link_type': ...,
-        'sex': ...,
-        'date_of_birth': ...,
-        'photo_url': ...,
-    }
-
-
-@pytest.fixture
-def user_fixture(user_fixture_data):
-    return UserFactory(**user_fixture_data)
+@pytest.fixture()
+def user_fixture():
+    return UserFactory()
