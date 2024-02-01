131015219d6887ecedb808b913712a5b81f58d01
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-01 12:47:21 -0300

    test: enhance tests' factories management for Campus and User models

--- users/tests/test_campus_model.py
+++ users/tests/test_campus_model.py
@@ -2,39 +2,36 @@ from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 from pytest import raises as assert_raises
 
-from users.tests.factories.factories_users import CampusFactory
-
 
 def test_campus_model_acronym_can_be_overwritten(db, campus_fixture):
-    campus_fixture.acronym = 'BR'
-    assert campus_fixture.acronym == 'BR'
+    campus = campus_fixture(acronym='BR')
+    assert campus.acronym == 'BR'
 
 
 def test_campus_model_acronym_has_max_length_4(db, campus_fixture):
-    campus_fixture.acronym = 'A' * 5
+    campus = campus_fixture(acronym='A' * 5)
     with assert_raises(ValidationError):
-        campus_fixture.full_clean()
+        campus.full_clean()
 
 
-def test_campus_model_name_is_unique(db):
+def test_campus_model_name_is_unique(db, campus_fixture):
     with assert_raises(IntegrityError):
-        CampusFactory(name='Currais Novos')
-        CampusFactory(name='Currais Novos')
+        campus1 = campus_fixture(name='Currais novos')
+        campus2 = campus_fixture(name='Currais Novos')
 
 
 def test_campus_model_name_has_max_length_50(db, campus_fixture):
-    campus_fixture.name = 'A' * 51
+    campus = campus_fixture(name='A' * 51)
     with assert_raises(ValidationError):
-        campus_fixture.full_clean()
+        campus.full_clean()
 
 
 def test_campus_model_name_cannot_be_null(db, campus_fixture):
-    campus_fixture.name = None
-    with assert_raises(ValidationError):
-        campus_fixture.full_clean()
+    with assert_raises(IntegrityError):
+        campus = campus_fixture(name=None)
 
 
 def test_campus_model_name_cannot_be_blank(db, campus_fixture):
-    campus_fixture.name = ''
+    campus = campus_fixture(name='')
     with assert_raises(ValidationError):
-        campus_fixture.full_clean()
+        campus.full_clean()
--- users/tests/test_user_model.py
+++ users/tests/test_user_model.py
@@ -1,11 +1,10 @@
-import pytest
 from django.core.exceptions import ValidationError
 from pytest import raises as assert_raises
 
-from users.tests.factories.factories_users import UserFactory
+from users.tests.factories import UserFactory
 
 
-def test_user_model_registration_has_max_length_14(db):
+def test_user_model_registration_has_max_length_14(db, user_fixture):
     user = UserFactory(registration='A' * 20)
     with assert_raises(ValidationError):
         user.full_clean()
