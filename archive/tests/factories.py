01002d24ad244ac05538344f3b5cad3f3e7d1e30
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-02 09:59:30 -0300

    test: update `CollectionFactory` fields

--- archive/tests/factories.py
+++ archive/tests/factories.py
@@ -1,6 +1,9 @@
-from factory import SubFactory
+from factory import RelatedFactory, SubFactory
 from factory.django import DjangoModelFactory, ImageField
 from factory.faker import faker
+
+from home.tests.factories import TagFactory
+from users.tests.factories import AdministratorFactory
 
 fake = faker.Faker('pt_BR')
 
@@ -9,12 +12,13 @@ class CollectionFactory(DjangoModelFactory):
     class Meta:
         model = 'archive.Collection'
 
-    administrator = SubFactory('users.Administrator')  # Add later
-    title = 'This is a placeholder title.'
-    cover = ImageField(filename='test_image')
-    slug = 'this-is-a-placeholder-title'
-    created_at = '05/06/2024'
-    updated_at = '05/07/2024'
+    administrator = SubFactory(AdministratorFactory)  # Add later
+    title = fake.pystr(max_chars=200)
+    cover = ImageField()
+    slug = fake.slug()
+    created_at = fake.date()
+    updated_at = fake.date()
+    tags = SubFactory(TagFactory)
 
 
 class FileFactory(DjangoModelFactory):
