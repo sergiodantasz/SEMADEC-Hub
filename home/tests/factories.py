55526619c7d096df99240d515cd1fa0547c9e05c
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-02 13:27:43 -0300

    test: add `django_get_or_create` field on TagFactory to deal with Unique values

--- home/tests/factories.py
+++ home/tests/factories.py
@@ -8,6 +8,7 @@ fake = faker.Faker('pt_BR')
 class TagFactory(DjangoModelFactory):
     class Meta:
         model = 'home.Tag'
+        django_get_or_create = ['name', 'slug']
 
     name = fake.pystr(max_chars=50)
     slug = fake.pystr(max_chars=75)
