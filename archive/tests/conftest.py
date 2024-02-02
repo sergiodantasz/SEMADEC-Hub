1be435b2a7b5255b38a482872ba09a0cc8ac8f68
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-02 13:26:02 -0300

    fix: change `collection_fixture` to receive ManyToMany fields

--- archive/tests/conftest.py
+++ archive/tests/conftest.py
@@ -1,11 +1,18 @@
 import pytest
 
 from archive.tests.factories import CollectionFactory, FileFactory
+from home.tests.conftest import tag_fixture
 
 
 @pytest.fixture
-def collection_fixture():
-    return CollectionFactory
+def collection_fixture(tag_fixture):  # noqa: F811
+    tag1 = tag_fixture()
+
+    def inner(title):
+        factory = CollectionFactory(tags=(tag1,), title=title)
+        return factory
+
+    return inner
 
 
 @pytest.fixture
