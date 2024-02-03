7e8d5cf98cd5ed068f28c1a49c3a632e61029c51
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-02 21:07:02 -0300

    test: add test for uniqueness of collection model's title

--- archive/tests/test_collection_model.py
+++ archive/tests/test_collection_model.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 from pytest import raises as assert_raises
 
 
@@ -6,3 +7,10 @@ def test_collection_model_title_has_max_length_200(db, collection_fixture):
     collection = collection_fixture(title='a' * 201)
     with assert_raises(ValidationError):
         collection.full_clean()
+
+
+def test_collection_model_title_is_unique(db, collection_fixture):
+    with assert_raises(IntegrityError):
+        col1 = collection_fixture(title='Coleção teste')
+        col2 = collection_fixture(title='Coleção Teste')
+        ...
