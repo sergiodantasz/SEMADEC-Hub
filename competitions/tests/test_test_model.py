575c6861c5b547f0efa4ca1ae00a55442a525f8a
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-06 13:59:22 -0300

    test: update `test_test_model_title_has_max_length_50` test

--- competitions/tests/test_test_model.py
+++ competitions/tests/test_test_model.py
@@ -4,7 +4,6 @@ from pytest import raises as assert_raises
 
 
 def test_test_model_title_has_max_length_50(db, test_fixture):  # Review it later
+    reg = test_fixture(title='a' * 51)
     with assert_raises(ValidationError):
-        reg = test_fixture(title='a' * 49)
         reg.full_clean()
-        ...
