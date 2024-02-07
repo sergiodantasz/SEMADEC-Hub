594ddb1fc65c9455fc12512a9c42b4c650524ad6
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-07 09:35:22 -0300

    test: add tests for `Test` model

--- competitions/tests/test_test_model.py
+++ competitions/tests/test_test_model.py
@@ -7,3 +7,45 @@ def test_test_model_title_has_max_length_50(db, test_fixture):  # Review it late
     reg = test_fixture(title='a' * 51)
     with assert_raises(ValidationError):
         reg.full_clean()
+
+
+def test_test_model_title_is_unique(db, test_fixture):
+    with assert_raises(IntegrityError):
+        reg1 = test_fixture(title='teste titulo')
+        reg2 = test_fixture(title='teste titulo')
+
+
+def test_test_model_title_cannot_be_null(db, test_fixture):
+    with assert_raises(IntegrityError):
+        reg = test_fixture(title=None)
+
+
+def test_test_model_title_cannot_be_blank(db, test_fixture):
+    reg = test_fixture(title='')
+    with assert_raises(ValidationError):
+        reg.full_clean()
+
+
+def test_test_model_description_cannot_be_null(db, test_fixture):
+    with assert_raises(IntegrityError):
+        reg = test_fixture(description=None)
+
+
+def test_test_model_description_can_be_blank(db, test_fixture):
+    reg = test_fixture(description='')
+    assert reg.description == ''
+
+
+def test_test_model_description_default_value_is_blank(db, test_fixture):
+    reg = test_fixture()
+    assert reg.description == ''
+
+
+def test_test_model_date_time_can_be_null(db, test_fixture):
+    reg = test_fixture(date_time=None)
+    assert reg.date_time is None
+
+
+def test_test_model_date_time_default_value_is_none(db, test_fixture):
+    reg = test_fixture()
+    assert reg.date_time is None
