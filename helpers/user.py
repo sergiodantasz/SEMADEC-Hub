265ba08c9e8cd625aa09fcc81aa3e52f04443733
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-07 14:12:56 -0300

    docs: add docstring to `format_photo_url` function

--- helpers/user.py
+++ helpers/user.py
@@ -2,6 +2,14 @@ from re import sub
 
 
 def format_photo_url(url: str) -> str:
+    """Format the user's photo URL.
+
+    Args:
+        url (str): photo URL.
+
+    Returns:
+        str: formatted photo URL.
+    """
     if not isinstance(url, str):
         raise TypeError('The URL must be a string.')
     new_url = 'https://suap.ifrn.edu.br' + sub(r'[0-9]{2,3}x[0-9]{3}\/', '', url)
