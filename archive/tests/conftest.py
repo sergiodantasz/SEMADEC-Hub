62646899dc9e13ee8c3640d7dd9005eb7707ec13
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-07 15:53:08 -0300

    test: update fixtures from `competitions` , `archive` and `editions` apps

--- archive/tests/conftest.py
+++ archive/tests/conftest.py
@@ -7,7 +7,6 @@ from home.tests.conftest import tag_fixture
 @pytest.fixture
 def collection_fixture(tag_fixture):  # noqa: F811
     def inner(**kwargs):
-        # ...
         factory = CollectionFactory.create(tags=(tag_fixture(),), **kwargs)
         return factory
 
