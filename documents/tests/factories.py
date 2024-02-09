5f87f062f5dff2522f2ad9419ae2f8720cc8bec7
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-08 21:30:08 -0300

    test: add `Document` model fields into `DocumentFactory`

--- documents/tests/factories.py
+++ documents/tests/factories.py
@@ -1,6 +1,8 @@
-from factory import SubFactory
-from factory.django import DjangoModelFactory
+from factory import Sequence, SubFactory, post_generation
+from factory.django import DjangoModelFactory, FileField
 from factory.faker import faker
+
+from users.tests.factories import AdministratorFactory
 
 fake = faker.Faker('pt_BR')
 
@@ -8,3 +10,20 @@ fake = faker.Faker('pt_BR')
 class DocumentFactory(DjangoModelFactory):
     class Meta:
         model = 'documents.Document'
+
+    administrator = SubFactory(AdministratorFactory)
+    title = fake.text(max_nb_chars=200)
+    content = FileField()
+    slug = Sequence(lambda x: fake.unique.slug())
+    created_at = fake.date()
+    updated_at = fake.date()
+
+    @post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        self.tags.add(*extracted)
+
+    @post_generation
+    def clear_unique(self, *args):
+        fake.unique.clear()
