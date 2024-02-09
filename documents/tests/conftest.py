a4d09a8f2b574beb80adf3d5b0f1d55fbd8a2f8c
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-08 21:29:34 -0300

    test: change `document_fixture` fixture to allow Many-To-Many relations with `tag_fixture`

--- documents/tests/conftest.py
+++ documents/tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
 
 from documents.tests.factories import DocumentFactory
+from home.tests.conftest import tag_fixture
 
 
 @pytest.fixture
-def document_fixture():
-    return DocumentFactory
+def document_fixture(tag_fixture):  # noqa: F811
+    def inner(**kwargs):
+        factory = DocumentFactory.create(tags=(tag_fixture(),), **kwargs)
+        return factory
+
+    return inner
