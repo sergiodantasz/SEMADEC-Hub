c396d3fa28f120180f39bafa9f110d3f9916f188
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-09 10:55:05 -0300

    refactor: refactor `remove_migrations` function

--- utils/remove_migrations.py
+++ utils/remove_migrations.py
@@ -1,5 +1,4 @@
-from os import remove, walk
-from os.path import join
+from os import remove
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,14 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 def remove_migrations() -> None:
     """Remove all files in the "migrations" folders, with the exception of __init__.py"""
-    for root, dirs, files in walk(BASE_DIR):
-        if root.endswith('migrations') and 'venv' not in root:
-            for file in files:
-                if file == '__init__.py':
-                    continue
-                file_path = join(root, file)
-                remove(file_path)
-                print(f'File removed: {file_path}')
+    all_files = list(BASE_DIR.glob('*/migrations/*.py'))
+    files_to_exclude = filter(lambda x: x.name != '__init__.py', all_files)
+    for file in files_to_exclude:
+        remove(str(file))
+        print(f'File removed: {file}')
 
 
 if __name__ == '__main__':
