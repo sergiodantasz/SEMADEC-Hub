67915d52f313f2ee3f24403ed6e6a60ba5e18296
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-12 12:19:24 -0300

    test: add dummy data field into `NewsFactory`

--- news/tests/factories.py
+++ news/tests/factories.py
@@ -1,6 +1,8 @@
-from factory import SubFactory
-from factory.django import DjangoModelFactory
+from factory import Sequence, SubFactory, post_generation
+from factory.django import DjangoModelFactory, ImageField
 from factory.faker import faker
+
+from users.tests.factories import AdministratorFactory
 
 fake = faker.Faker('pt_BR')
 
@@ -8,3 +10,13 @@ fake = faker.Faker('pt_BR')
 class NewsFactory(DjangoModelFactory):
     class Meta:
         model = 'news.News'
+
+    administrator = SubFactory(AdministratorFactory)
+    title = fake.text(max_nb_chars=200)
+    excerpt = fake.text(max_nb_chars=200)
+    content = fake.text()
+    slug = Sequence(lambda x: fake.unique.slug())
+
+    @post_generation
+    def clear_unique(self, *args):
+        fake.unique.clear()
