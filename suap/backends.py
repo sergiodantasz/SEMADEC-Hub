6d8857bf71c274bb803a6c614b520fc8d45c0846
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-02-23 20:53:51 -0300

    refactor: optimize backend for user login

--- suap/backends.py
+++ suap/backends.py
@@ -1,9 +1,43 @@
+from dataclasses import asdict, dataclass
+from io import BytesIO
+from os.path import splitext
+from re import sub
+
+from django.core.files.images import ImageFile
+from requests import get
 from social_core.backends.oauth import BaseOAuth2
 
 from editions.models import Course
-from helpers.model import update_model_fields
 from helpers.user import create_emails, download_photo
 from users.models import Campus, User
+
+
+@dataclass(init=False)
+class UserData:
+    registration: str
+    campus: str
+    course: str
+    full_name: str
+    first_name: str
+    first_name: str
+    last_name: str
+    cpf: str
+    link_type: str
+    sex: str
+    date_of_birth: str
+    photo: ImageFile
+
+    def __init__(self, response):
+        self.registration = response.get('identificacao')
+        self.campus = Campus.objects.filter(acronym=response.get('campus')).first()
+        self.course = Course.objects.filter(name=response.get('course')).first()
+        self.full_name = response.get('nome_social') or response.get('nome_registro')
+        self.first_name, *_, self.last_name = self.full_name.split()
+        self.cpf = response.get('cpf')
+        self.link_type = response.get('tipo_usuario')
+        self.sex = response.get('sexo')
+        self.date_of_birth = response.get('data_de_nascimento')
+        self.photo = download_photo(response.get('foto'), self.registration)
 
 
 class SuapOAuth2(BaseOAuth2):
@@ -36,29 +70,10 @@ class SuapOAuth2(BaseOAuth2):
         return response
 
     def get_user_details(self, response):
-        registration = response.get('identificacao')
-        full_name = response.get('nome_registro')
-        if social_name := response.get('nome_social'):
-            full_name = social_name
-        first_name, *_, last_name = full_name.split()
-        campus = Campus.objects.filter(acronym=response.get('campus')).first()
-        course = Course.objects.filter(name=response.get('course')).first()
-        photo = download_photo(response.get('foto'), registration)
-        user_data = {
-            'registration': registration,
-            'full_name': full_name,
-            'first_name': first_name,
-            'last_name': last_name,
-            'cpf': response.get('cpf'),
-            'campus': campus,
-            'course': course,
-            'sex': response.get('sexo'),
-            'link_type': response.get('tipo_usuario'),
-            'date_of_birth': response.get('data_de_nascimento'),
-            'photo': photo,
-        }
-        if not User.objects.filter(registration=registration).exists():
-            user = User.objects.create(**user_data)
+        user_data = UserData(response)
+        user_reg = User.objects.filter(registration=user_data.registration)
+        if not user_reg.exists():
+            user = User.objects.create(**asdict(user_data))
             create_emails(
                 user,
                 response.get('email_secundario'),
@@ -66,18 +81,15 @@ class SuapOAuth2(BaseOAuth2):
                 response.get('email_academico'),
             )
         else:
-            user = User.objects.get(registration=registration)
-            update_model_fields(user, user_data, ['photo'])
-            if not user.photo.storage.exists(user.photo.name):
-                setattr(user, 'photo', photo)
-                user.save()
-            elif photo.read() != user.photo.read():  # Refactor later
-                user.photo.delete()
-                setattr(user, 'photo', photo)
-                user.save()
+            user_reg = user_reg.first()
+            test = asdict(user_data)
+            for k, v in test.items():
+                if getattr(user_reg, k) != getattr(user_data, k):
+                    setattr(user_reg, k, v)
+            user_reg.save()
         return {
-            'username': registration,
-            'first_name': first_name,
-            'last_name': last_name,
+            'username': user_data.registration,
+            'first_name': user_data.first_name,
+            'last_name': user_data.last_name,
             'email': response.get('email_preferencial'),
         }
