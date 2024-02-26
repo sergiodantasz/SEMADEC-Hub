b8da0f319d1c59f84cc139f7433d04802d7ab621
Sérgio Dantas <sergiodnts828@gmail.com>
2024-02-26 12:22:22 -0300

    feat: create documents page

--- documents/urls.py
+++ documents/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from documents import views
+
 app_name = 'documents'
 
 urlpatterns = [
-    path('', lambda request: '<h1>TEST</h1>', name='documents'),
+    path('', views.documents, name='documents'),
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from users.models import User
+
+
+def documents(request):
+    context = {'title': 'Documentos'}
+    if request.user.is_authenticated:
+        user = User.objects.get(registration=request.user.username)
+        context['user'] = user  # type: ignore
+    return render(request, 'documents/pages/documents.html', context)
