ff7b250127a9a9e51128b2b2245dc63dbf0aba46
Sérgio Dantas <sergiodnts828@gmail.com>
2024-03-06 18:59:16 -0300

    feat: create error pages

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'home',
     'users',
     'news',
+    'handlers',
 ]
 
 MIDDLEWARE = [
---
+++ handlers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HandlersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'handlers'
