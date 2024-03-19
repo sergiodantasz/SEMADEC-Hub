b7ce6e0c3af5de406df9b02e1d0e9d02e2bf9f73
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-03-19 09:46:44 -0300

    feat: add `login_required` decorator on `news` view

--- news/views.py
+++ news/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from users.models import User
 
 
+@login_required(login_url='/login/')
 def news(request):
     context = {'title': 'Notícias'}
     if request.user.is_authenticated:
