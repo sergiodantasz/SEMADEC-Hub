29b2363385bce99940f32af6a62dee696e6012be
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-03-18 23:19:21 -0300

    feat: implement dynamically generated data to be shown on archive page

--- archive/tests/factories.py
+++ archive/tests/factories.py
@@ -10,6 +10,21 @@ from users.tests.factories import AdministratorFactory
 fake = faker.Faker('pt_BR')
 
 
+class FileFactory(DjangoModelFactory):
+    class Meta:
+        model = 'archive.File'
+        skip_postgeneration_save = True
+
+    display_name = fake.pystr(max_chars=225)
+    content = Sequence(lambda x: fake.unique.file_path())
+
+    # post_generation(fake.unique.clear())
+
+
+class ImageFactory(FileFactory):
+    content = ImageField()
+
+
 class CollectionFactory(DjangoModelFactory):
     class Meta:
         model = 'archive.Collection'
@@ -17,19 +32,18 @@ class CollectionFactory(DjangoModelFactory):
 
     administrator = SubFactory(AdministratorFactory)  # Add later
     files = PostGeneration(lambda obj, create, extracted: obj.files)
-    title = Sequence(lambda x: fake.unique.pystr(max_chars=200))
+    title = Sequence(lambda x: fake.unique.text(max_nb_chars=200))
     type = FuzzyChoice(Collection.COLLECTION_TYPE_CHOICES)
     slug = Sequence(lambda x: fake.unique.slug())
     tags = PostGeneration(lambda obj, create, extracted: obj.tags)
     post_generation(fake.unique.clear())
 
 
-class FileFactory(DjangoModelFactory):
-    class Meta:
-        model = 'archive.File'
-        skip_postgeneration_save = True
+class CollectionArchiveFactory(CollectionFactory):
+    type = 'image'
 
-    display_name = fake.pystr(max_chars=225)
-    content = fake.unique.file_path()
-
-    post_generation(fake.unique.clear())
+    @post_generation
+    def files(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        self.files.add(*extracted)
--- archive/views.py
+++ archive/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+from archive.tests.factories import (
+    CollectionArchiveFactory,
+    ImageFactory,
+)
 from users.models import User
 
 
 def archive(request):
     context = {'title': 'Acervo'}
+    archive_regs = CollectionArchiveFactory.create_batch(
+        size=3,
+        files=(
+            ImageFactory(),
+            ImageFactory(),
+            ImageFactory(),
+        ),
+    )
     if request.user.is_authenticated:
         user = User.objects.get(registration=request.user.username)
         context['user'] = user  # type: ignore
+        context['archive_regs'] = archive_regs
     return render(request, 'archive/pages/archive.html', context)
