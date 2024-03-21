b33efdee0c958171d2daf7cf05ca38eb7bf3b53e
Sérgio Dantas <sergiodnts828@gmail.com>
2024-03-21 11:20:10 -0300

    chore: create function to set user permissions

--- helpers/user.py
+++ helpers/user.py
@@ -5,6 +5,8 @@ from re import sub
 from django.core.files.images import ImageFile
 from requests import get
 
+from users.models import User
+
 
 def format_photo_url(url: str) -> str:
     """Format the user's photo URL.
@@ -40,3 +42,27 @@ def download_photo(url: str, user_registration: str) -> ImageFile:
     photo_name = user_registration + splitext(photo_url)[-1]
     photo = ImageFile(BytesIO(photo_data), photo_name)
     return photo
+
+
+def set_permissions(
+    user_registration: str,
+    *,
+    admin: bool = False,
+    staff: bool = False,
+    superuser: bool = False,
+) -> None:
+    if not isinstance(user_registration, str):
+        raise TypeError('The user registration must be a string.')
+    if not isinstance(admin, bool):
+        raise TypeError('The admin must be a boolean.')
+    if not isinstance(staff, bool):
+        raise TypeError('The staff must be a boolean.')
+    if not isinstance(superuser, bool):
+        raise TypeError('The superuser must be a boolean.')
+    user = User.objects.filter(registration=user_registration).first()
+    if user is None:
+        raise ValueError('This user registration does not exist.')
+    setattr(user, 'is_admin', admin)
+    setattr(user, 'is_staff', staff)
+    setattr(user, 'is_superuser', superuser)
+    user.save()
