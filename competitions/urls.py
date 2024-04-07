6de317b69a9d911d2ab696916dfae1935c005c6c
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-04-07 10:41:29 -0300

    feat: update competition-related components

--- competitions/urls.py
+++ competitions/urls.py
@@ -4,8 +4,11 @@ from competitions import views
 
 app_name = 'competitions'
 
+
 urlpatterns = [
     path('', views.competitions, name='competitions'),
     path('esportes/', views.sports, name='sports'),
+    path('esportes/busca', views.sports_search, name='sports_search'),
     path('provas/', views.tests, name='tests'),
+    path('provas/busca', views.tests_search, name='tests_search'),
 ]
--- competitions/views.py
+++ competitions/views.py
@@ -1,8 +1,10 @@
 from random import choices
 
+from django.db.models import Q
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from competitions.models import Sport, Test
 from competitions.tests.factories import CategoryFactory, SportFactory, TestFactory
 
 
@@ -11,20 +13,60 @@ def competitions(request):
 
 
 def sports(request):
-    context = {'title': 'Competições'}
-    cats = CategoryFactory.create_batch(size=3)
-    competition_regs = SportFactory.create_batch(
+    cats = CategoryFactory.create_batch(size=3)  # Remove if needed
+    SportFactory.create_batch(
         size=4,
         categories=choices(cats, k=1),
-    )
-    context['competition_type'] = 'sports'
-    context['competition_regs'] = competition_regs
+    )  # Remove if needed
+    context = {
+        'title': 'Competições',
+        'page_variant': 'sports',
+        'page_content': Sport.objects.all(),
+        'search_namespace': reverse('competitions:sports_search'),
+    }
+    return render(request, 'competitions/pages/competitions.html', context)
+
+
+def sports_search(request):
+    querystr = request.GET.get('q').strip()
+
+    if not querystr:
+        return redirect(reverse('competitions:sports'))
+
+    page_content = Sport.objects.filter(
+        name__icontains=querystr,
+    ).order_by('name')
+    context = {
+        'title': 'Competições',
+        'page_variant': 'sports',
+        'page_content': page_content,
+    }
     return render(request, 'competitions/pages/competitions.html', context)
 
 
 def tests(request):
-    context = {'title': 'Competições'}
-    competition_regs = TestFactory.create_batch(size=4)
-    context['competition_type'] = 'tests'
-    context['competition_regs'] = competition_regs
+    TestFactory.create_batch(size=4)  # Remove if needed
+    context = {
+        'title': 'Competições',
+        'page_variant': 'tests',
+        'page_content': Test.objects.all(),
+        'search_namespace': reverse('competitions:tests_search'),
+    }
+    return render(request, 'competitions/pages/competitions.html', context)
+
+
+def tests_search(request):
+    querystr = request.GET.get('q').strip()
+
+    if not querystr:
+        return redirect(reverse('competitions:tests'))
+
+    page_content = Test.objects.filter(
+        Q(Q(title__icontains=querystr) | Q(description__icontains=querystr))
+    ).order_by('title')
+    context = {
+        'title': 'Competições',
+        'page_variant': 'tests',
+        'page_content': page_content,
+    }
     return render(request, 'competitions/pages/competitions.html', context)
