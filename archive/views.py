645419fce527f8da2b4b22544406aa31179fc1c0
Sérgio Dantas <sergiodnts828@gmail.com>
2024-04-09 18:36:21 -0300

    fix: remove archive app errors

--- archive/views.py
+++ archive/views.py
@@ -1,45 +1,11 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from archive.forms import SubmitArchiveForm
-from archive.models import Collection
-from archive.tests.factories import (
-    CollectionArchiveFactory,
-    ImageFactory,
-)
+from home.models import Collection
 
 
 def archive(request):
-    archive_regs = CollectionArchiveFactory.create_batch(
-        size=3,
-        files=(
-            ImageFactory(),
-            ImageFactory(),
-            ImageFactory(),
-        ),
-    )
     context = {
         'title': 'Acervo',
-        'archive_regs': archive_regs,
     }
     return render(request, 'archive/pages/archive.html', context)
-
-
-def archive_detailed(request, slug):
-    archive = get_object_or_404(Collection, slug=slug)
-    context = {'title': archive.title, 'archive_reg': archive}
-    return render(request, 'archive/pages/archive_detailed.html', context)
-
-
-def submit_archive(request):  # Change name to `archive_submit`
-    form = SubmitArchiveForm()
-    context = {'title': 'Acervo', 'form': form}
-    return render(request, 'archive/pages/submitarchive.html', context)
-
-
-def create_archive(request):  # Change name to `archive_create`
-    if not request.POST:
-        raise Http404()
-    form = SubmitArchiveForm(request.POST)
-    context = {'title': 'Acervo', 'form': form}
-    return render(request, 'archive/pages/submitarchive.html', context)
