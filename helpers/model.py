37ab0209a85ef16e7498da4d606c4a9ef2f851d4
Sérgio Dantas <sergiodnts828@gmail.com>
2024-04-09 16:15:41 -0300

    feat: create is_owner and generate_collection_cover_path functions

--- helpers/model.py
+++ helpers/model.py
@@ -1,4 +1,8 @@
+from os.path import split
 from typing import Any
+
+from home.models import Collection
+from users.models import User
 
 
 def get_object(model, *args, **kwargs) -> Any | None:
@@ -16,5 +20,28 @@ def get_object(model, *args, **kwargs) -> Any | None:
     return objects.first()
 
 
-def is_owner(user, registry) -> bool:
+def is_owner(user: User, registry: Any) -> bool:
+    """Receive a user object and a registry and validate is this user created this registry.
+
+    Args:
+        user (User): the user object.
+        registry (Any): the registry.
+
+    Returns:
+        bool: True if user created the registry, otherwise False.
+    """
     return user == registry.administrator
+
+
+def generate_collection_cover_path(instance: Collection, filename: str) -> str:
+    """Generate cover path of a collection depending on its type.
+
+    Args:
+        instance (Collection): collection instance.
+        filename (str): file name.
+
+    Returns:
+        str: cover path based on the collection type.
+    """
+    _, filename = split(filename)
+    return f'collections/{instance.collection_type}/covers/{filename}'
