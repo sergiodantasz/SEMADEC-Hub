6b16493605a3a01f457bf54b3f38701328ea6897
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-04-10 14:40:13 -0300

    refactor: update competitions search url

--- competitions/urls.py
+++ competitions/urls.py
@@ -8,7 +8,7 @@ app_name = 'competitions'
 urlpatterns = [
     path('', views.competitions, name='competitions'),
     path('esportes/', views.sports, name='sports'),
-    path('esportes/busca', views.sports_search, name='sports_search'),
+    path('esportes/buscar', views.sports_search, name='sports_search'),
     path('provas/', views.tests, name='tests'),
-    path('provas/busca', views.tests_search, name='tests_search'),
+    path('provas/buscar', views.tests_search, name='tests_search'),
 ]
