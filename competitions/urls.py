49a9e04cac85174ac0f4d0a2edf9fa166f04481e
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-04-22 18:32:02 -0300

    feat: update urls for competitions app

--- competitions/urls.py
+++ competitions/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('esportes/', views.sports, name='sports'),
     path('esportes/buscar', views.sports_search, name='sports_search'),
     path('esportes/criar', views.sports_create, name='sports_create'),
+    path('esportes/editar', views.sports_edit, name='sports_edit'),
     path('provas/', views.tests, name='tests'),
     path('provas/buscar', views.tests_search, name='tests_search'),
     path('provas/criar', views.tests_create, name='tests_create'),
+    path('provas/editar', views.tests_edit, name='tests_edit'),
 ]
