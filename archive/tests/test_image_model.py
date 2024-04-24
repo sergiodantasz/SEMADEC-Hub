6336952feb2c88276cd881e9d1dfc941e8b14522
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-04-24 11:49:44 -0300

    test: add tests for `Image` model

---
+++ archive/tests/test_image_model.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from pytest import raises as assert_raises
+
+from archive.models import Image
+from home.models import Collection
+
+
+def test_image_model_collection_fk_points_to_collection_model(db, image_fixture):
+    reg = image_fixture()[0]
+    assert isinstance(reg.collection, Collection)
+
+
+def test_image_model_content_path_is_correct(db, image_fixture):
+    reg = image_fixture()[0]
+    assert 'collections/image/files' in reg.content.url
+
+
+def test_image_dunder_str_method_returns_image_content_name(db, image_fixture):
+    reg = image_fixture()[0]
+    assert str(reg) == reg.content.name
