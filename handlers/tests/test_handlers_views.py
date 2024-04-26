85eb956b8805663488956fe6c371c16888a6edef
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-04-26 08:34:06 -0300

    test: add tests to handlers

---
+++ handlers/tests/test_handlers_views.py
@@ -0,0 +1,13 @@
+from django.http import Http404
+from django.test import RequestFactory
+from django.urls import resolve
+from pytest import mark
+
+from handlers import views
+
+
+@mark.skip
+def test_handler_view_page_not_found_404_loads_correct_view():
+    request = RequestFactory().get('/')
+    view = views.page_not_found_404(request, Http404)
+    ...
