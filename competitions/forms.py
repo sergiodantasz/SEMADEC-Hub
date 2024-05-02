cac0b5d7574d74459d98ec995e458bdaaa424c5f
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-05-02 09:00:41 -0300

    feat: add forms for `MatchForm` and `MatchTeamForm` models

--- competitions/forms.py
+++ competitions/forms.py
@@ -1,6 +1,16 @@
+from tkinter import Widget
+
 from django import forms
 
-from competitions.models import Category, Sport, Test, TestTeam
+from competitions.models import (
+    Category,
+    Match,
+    MatchTeam,
+    Sport,
+    SportCategory,
+    Test,
+    TestTeam,
+)
 from editions.models import Team
 from helpers.form import set_attr, set_placeholder
 
@@ -74,6 +84,66 @@ class TestTeamForm(forms.ModelForm):
 
     class Meta:
         model = TestTeam
+        fields = ['score']
+
+    score = forms.CharField(
+        label='Pontuação',
+        widget=forms.NumberInput(),
+    )
+
+
+class MatchForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs) -> None:
+        edition_obj = kwargs.get('edition_obj', '')
+        if edition_obj:
+            kwargs.pop('edition_obj')
+        super().__init__(*args, **kwargs)
+        if edition_obj:
+            sport_category_ids = edition_obj.sports.values_list(
+                'sport_category', flat=True
+            )
+            sport_category_queryset = SportCategory.objects.filter(
+                pk__in=sport_category_ids
+            )
+            self.fields['teams'].queryset = edition_obj.teams.all()
+            self.fields['sport_category'].queryset = sport_category_queryset
+
+    class Meta:
+        model = Match
+        fields = ['sport_category', 'date_time', 'teams']
+
+    sport_category = forms.ModelChoiceField(
+        queryset=SportCategory.objects.all(),  # Review it later
+        widget=forms.RadioSelect(
+            attrs={'class': 'input-radio-list'},
+        ),
+        label='Esporte',
+    )
+
+    teams = forms.ModelMultipleChoiceField(
+        queryset=Team.objects.all(),  # Review it later
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'class': 'input-checkbox-list'},
+        ),
+        label='Times',
+    )
+    date_time = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={
+                'type': 'datetime-local',
+                'class': 'input-datetime',
+            }
+        ),
+        label='Horário',
+    )
+
+
+class MatchTeamForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = MatchTeam
         fields = ['score']
 
     score = forms.CharField(
