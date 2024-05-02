76254306aaa7a422913ca3e24377dc9b949bbdf8
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-05-02 08:57:42 -0300

    chore: update windows script files

--- scripts/windows/all.ps1
+++ scripts/windows/all.ps1
@@ -1,6 +1,12 @@
+param(
+    [switch]$r
+)
+
 .\scripts\windows\removemigrations.ps1
 .\scripts\windows\removedata.ps1
 .\scripts\windows\makemigrations.ps1
 .\scripts\windows\migrate.ps1
 .\scripts\windows\collectstatic.ps1
-.\scripts\windows\runserver.ps1
+if (!$r) {
+  .\scripts\windows\runserver.ps1
+}
