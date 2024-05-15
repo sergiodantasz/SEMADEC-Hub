32fc0230e85606e7ecb5b460f90aaa00c316ab91
Sérgio Dantas <sergiodnts828@gmail.com>
2024-05-15 16:43:59 -0300

    feat: create archive page

--- archive/static/archive/js/archive-item-modal.js
+++ archive/static/archive/js/archive-item-modal.js
@@ -1,17 +1,25 @@
-let containerOuter = document.querySelector('.images-container');
-let containerInner = containerOuter.querySelectorAll('.archive-image-wrapper');
-let modal = document.querySelector('.archive-modal');
-let modalImage = document.querySelector('.archive-modal-image');
-let closeButton = document.getElementById('archive-modal-button-close')
+let dialog = document.querySelector(".dialog");
+let dialogImage = dialog.querySelector(".dialog-image");
 
-containerInner.forEach(function(container) {
-  container.addEventListener('click', function(event) {
-    let containerClicked = event.target;
-    modalImage.src = containerClicked.src;
-    modal.showModal();
+let closeButton = document.getElementById("dialog-button-close");
+let downloadButton = document.getElementById("dialog-button-download");
+let deleteButton = document.getElementById("dialog-button-delete");
+
+let imageContainers = document.querySelectorAll(".archive-image-wrapper");
+
+const imageDeleteUrl = "/acervo/imagem/apagar/";
+
+imageContainers.forEach((imageContainer) => {
+  imageContainer.addEventListener("click", function (e) {
+    const image = e.target;
+    dialogImage.src = image.src;
+    const imagePk = this.getAttribute("pk");
+    deleteButton?.setAttribute("href", imageDeleteUrl.concat(imagePk, "/"));
+    downloadButton.setAttribute("href", image.src);
+    downloadButton.setAttribute("download", true);
+    dialog.showModal();
   });
 });
 
-closeButton.onclick = function () {
-  modal.close();
-}
+closeButton.addEventListener("click", (e) => dialog.close());
+deleteButton?.addEventListener("click", (e) => dialog.close());
