7a692bc1468692e1f499e6ed6f49f3cf7c894f7c
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-05-19 14:50:23 -0300

    test: update `test_archive_views` file

--- archive/tests/test_archive_views.py
+++ archive/tests/test_archive_views.py
@@ -6,21 +6,29 @@ from archive import views
 
 def test_archive_viewname_redirects_to_archive_view():
     view = resolve(reverse('archive:home'))
-    assert view.func is views.archive
+    assert view.func is views.archive_collection
 
 
 def test_archive_detailed_viewname_redirects_to_archive_detailed_view():
-    view = resolve(reverse('archive:archive_detailed', kwargs={'slug': 'test-slug'}))
-    assert view.func is views.archive_detailed
+    view = resolve(reverse('archive:detailed', kwargs={'slug': 'test-slug'}))
+    assert view.func is views.view_archive_collection
 
 
-@mark.skip
-def test_archive_submit_viewname_redirects_to_archive_submit_view():
-    view_new = resolve(reverse('archive:submitarchive'))
-    assert view_new.func is views.submit_archive
-
-
-@mark.skip
 def test_archive_create_viewname_redirects_to_archive_create_view():
-    view_new = resolve(reverse('archive:createarchive'))
-    assert view_new.func is views.create_archive
+    view_new = resolve(reverse('archive:create'))
+    assert view_new.func is views.create_archive_collection
+
+
+def test_archive_edit_viewname_redirects_to_archive_edit_view():
+    view_new = resolve(reverse('archive:edit', kwargs={'slug': 'test'}))
+    assert view_new.func is views.edit_archive_collection
+
+
+def test_archive_delete_viewname_redirects_to_archive_delete_view():
+    view_new = resolve(reverse('archive:delete', kwargs={'slug': 'test'}))
+    assert view_new.func is views.delete_archive_collection
+
+
+def test_archive_image_delete_viewname_redirects_to_archive_image_delete_view():
+    view_new = resolve(reverse('archive:image:delete', kwargs={'pk': 1}))
+    assert view_new.func is views.delete_image
