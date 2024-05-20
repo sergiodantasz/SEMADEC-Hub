7afdcca6500160ac11cac648a50dc30eae1a2fa1
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-05-20 20:51:54 -0300

    test: add test for forms of `teams` app

--- teams/tests/conftest.py
+++ teams/tests/conftest.py
@@ -1,12 +1,8 @@
 import pytest
 from django.conf import settings
 
+from teams.forms import ClassForm, CourseForm, TeamForm
 from teams.tests.factories import ClassFactory, CourseFactory, TeamFactory
-
-
-@pytest.fixture
-def class_fixture():
-    yield ClassFactory
 
 
 @pytest.fixture
@@ -15,8 +11,42 @@ def course_fixture():
 
 
 @pytest.fixture
+def course_form_fixture():
+    data = {
+        'name': 'test',
+    }
+    caller = lambda **kwargs: CourseForm(data=data | kwargs)  # noqa
+    yield caller
+
+
+@pytest.fixture
+def class_fixture():
+    yield ClassFactory
+
+
+@pytest.fixture
+def class_form_fixture():
+    data = {
+        'name': 'test',
+        'course': CourseFactory(),
+    }
+    caller = lambda **kwargs: ClassForm(data=data | kwargs)  # noqa
+    yield caller
+
+
+@pytest.fixture
 def team_fixture():
     yield TeamFactory
+
+
+@pytest.fixture
+def team_form_fixture():
+    data = {
+        'name': 'test',
+        'classes': ClassFactory.create_batch(3),
+    }
+    caller = lambda **kwargs: TeamForm(data=data | kwargs)  # noqa
+    yield caller
 
 
 if __name__.startswith('test'):
---
+++ teams/tests/test_course_forms.py
@@ -0,0 +1,89 @@
+from django.forms import CheckboxSelectMultiple
+from django.forms.widgets import RadioSelect
+
+from teams.models import Class, Course
+
+
+def test_course_form_is_valid(db, course_form_fixture):
+    form = course_form_fixture()
+    assert form.is_valid()
+
+
+def test_course_form_name_max_length_is_75(db, course_form_fixture):
+    form = course_form_fixture()
+    assert form.fields['name'].max_length == 75
+
+
+def test_course_form_name_label_is_correct(db, course_form_fixture):
+    form = course_form_fixture()
+    assert form.fields['name'].label == 'Nome'
+
+
+def test_class_form_is_valid(db, class_form_fixture):
+    form = class_form_fixture()
+    assert form.is_valid()
+
+
+def test_class_form_name_max_length_is_30(db, class_form_fixture):
+    form = class_form_fixture()
+    assert form.fields['name'].max_length == 30
+
+
+def test_class_form_name_label_is_correct(db, class_form_fixture):
+    form = class_form_fixture()
+    assert form.fields['name'].label == 'Nome'
+
+
+def test_class_form_course_label_is_correct(db, class_form_fixture):
+    form = class_form_fixture()
+    assert form.fields['course'].label == 'Curso'
+
+
+def test_class_form_course_widget_is_radioselect(db, class_form_fixture):
+    form = class_form_fixture()
+    assert isinstance(form.fields['course'].widget, RadioSelect)
+
+
+def test_class_form_course_widget_has_html_class(db, class_form_fixture):
+    form = class_form_fixture()
+    assert form.fields['course'].widget.attrs['class'] == 'input-radio-list'
+
+
+def test_class_form_course_queryset_is_correct(db, class_form_fixture):
+    form = class_form_fixture()
+    assert all(isinstance(reg, Course) for reg in form.fields['course'].queryset)
+
+
+def test_team_form_is_valid(db, team_form_fixture):
+    form = team_form_fixture()
+    assert form.is_valid()
+
+
+def test_team_form_name_max_length_is_75(db, team_form_fixture):
+    form = team_form_fixture()
+    assert form.fields['name'].max_length == 75
+
+
+def test_team_form_name_label_is_correct(db, team_form_fixture):
+    form = team_form_fixture()
+    assert form.fields['name'].label == 'Nome'
+
+
+def test_team_form_classes_queryset_is_correct(db, team_form_fixture):
+    form = team_form_fixture()
+    assert all(isinstance(reg, Class) for reg in form.fields['classes'].queryset)
+
+
+def test_team_form_classes_widget_is_checkboxmultiple(db, team_form_fixture):
+    form = team_form_fixture()
+    assert isinstance(form.fields['classes'].widget, CheckboxSelectMultiple)
+
+
+def test_team_form_classes_widget_has_html_class(db, team_form_fixture):
+    form = team_form_fixture()
+    assert form.fields['classes'].widget.attrs['class'] == 'input-checkbox-list'
+
+
+def test_team_form_classes_label_is_correct(db, team_form_fixture):
+    form = team_form_fixture()
+    assert form.fields['classes'].label == 'Turmas'
