d9fee1ae2792eb08a01bc7f57377c70b81ab4111
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-03 13:11:20 -0300

    feat: add content to `MessageMixin`

--- apps/home/views/views.py
+++ apps/home/views/views.py
@@ -12,6 +12,19 @@ class MessageMixin:
     success_message = ''
     error_message = ''
 
+    def is_model_populated(self, model):
+        if self.success_message:
+            messages.success(self.request, self.success_message)
+        elif self.error_message:
+            messages.success(self.request, self.error_message)
+        return model.objects.exists()
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        if self.success_message:
+            messages.success(self.request, self.success_message)
+        return response
+
     def form_valid(self, form):
         response = super().form_valid(form)
         if self.success_message:
