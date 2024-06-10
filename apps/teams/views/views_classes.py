4e3aad3e8c229ab2be0b73f035bf58ec3fe4eb4d
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-10 14:38:12 -0300

    refactor: refactor `ClassCreateView`

--- apps/teams/views/views_classes.py
+++ apps/teams/views/views_classes.py
@@ -11,6 +11,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import DeleteView, FormView, UpdateView
 from home.views import BaseListView, BaseSearchView, MessageMixin
 
+from apps.home.views.views import BaseCreateView
 from apps.teams.forms import ClassForm
 from apps.teams.models import Class, Course
 from helpers.decorators import admin_required
@@ -51,16 +52,16 @@ class ClassSearchView(BaseSearchView):
 
 @method_decorator(login_required, name='dispatch')
 @method_decorator(admin_required, name='dispatch')
-class ClassCreateView(MessageMixin, FormView):
+class ClassCreateView(BaseCreateView):
     form_class = ClassForm
     template_name = 'teams/pages/class-create.html'
-    success_url = reverse_lazy('teams:classes:home')
-    success_message = 'Turma adicionada com sucesso.'
-    error_message = 'Preencha os campos do formulário corretamente.'
-    error_message_course = 'Adicione ao menos um curso antes de criar uma turma.'
-
-    def is_model_populated(self, model: Model):
-        return model.objects.exists()
+    msg = {
+        'success': {'form': 'Turma adicionada com sucesso.'},
+        'error': {
+            'form': 'Preencha os campos do formulário corretamente.',
+            'course': 'Adicione ao menos um curso antes de criar uma turma.',
+        },
+    }
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -69,13 +70,13 @@ class ClassCreateView(MessageMixin, FormView):
 
     def get(self, request, *args, **kwargs):
         if not self.is_model_populated(Course):
-            messages.error(self.request, self.error_message_course)
-            return redirect(self.success_url)
+            messages.error(self.request, self.msg['error']['course'])
+            return redirect(self.get_success_url())
         context = self.get_context_data()
         return self.render_to_response(context)
 
     def form_valid(self, form):
-        form_reg = form.save()
+        form.save()
         return super().form_valid(form)
 
 
