ddee66c95f06ed0a1f4090b89cfe47ed5a914a77
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-10 11:24:02 -0300

    refactor: refactor `TeamCreateView`

--- apps/teams/views/views_teams.py
+++ apps/teams/views/views_teams.py
@@ -11,6 +11,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import DeleteView, FormView, UpdateView
 from home.views import BaseListView, BaseSearchView, MessageMixin
 
+from apps.home.views.views import BaseCreateView
 from apps.teams.forms import TeamForm
 from apps.teams.models import Class, Team
 from helpers.decorators import admin_required
@@ -48,16 +49,16 @@ class TeamSearchView(BaseSearchView):
         return context
 
 
-class TeamCreateView(MessageMixin, FormView):
+class TeamCreateView(BaseCreateView):
     form_class = TeamForm
     template_name = 'teams/pages/team-create.html'
-    success_url = reverse_lazy('teams:home')
-    success_message = 'Time adicionado com sucesso.'
-    error_message = 'Preencha os campos do formulário corretamente.'
-    error_message_class = 'Adicione ao menos uma turma antes de criar uma edição.'
-
-    def is_model_populated(self, model: Model):
-        return model.objects.exists()
+    msg = {
+        'success': {'form': 'Time adicionado com sucesso.'},
+        'error': {
+            'form': 'Preencha os campos do formulário corretamente.',
+            'class': 'Adicione ao menos uma turma antes de criar uma edição.',
+        },
+    }
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -66,13 +67,13 @@ class TeamCreateView(MessageMixin, FormView):
 
     def get(self, request, *args, **kwargs):
         if not self.is_model_populated(Class):
-            messages.error(self.request, self.error_message_class)
-            return redirect(self.success_url)
+            messages.error(self.request, self.messages['error']['class'])
+            return redirect(self.get_success_url())
         context = self.get_context_data()
         return self.render_to_response(context)
 
     def form_valid(self, form):
-        form_reg = form.save()
+        form.save()
         return super().form_valid(form)
 
 
