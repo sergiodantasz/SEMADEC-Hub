0d8073e1935092d1c2d24d1e92d6ff764ff3d873
Sérgio Dantas <sergiodnts828@gmail.com>
2024-06-11 04:22:08 -0300

    refactor: add docstring

--- scripts/removedata.py
+++ scripts/removedata.py
@@ -5,9 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def remove_data():
+    """Remove static and media dirs and db.sqlite3 file."""
     rmtree(BASE_DIR / 'static', ignore_errors=True)
     rmtree(BASE_DIR / 'media', ignore_errors=True)
     (BASE_DIR / 'core' / 'db.sqlite3').unlink(missing_ok=True)
+
 
 if __name__ == '__main__':
     remove_data()
--- scripts/removemigrations.py
+++ scripts/removemigrations.py
@@ -5,12 +5,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def remove_migrations():
+    """Remove all migration files inside the apps."""
     files = list(BASE_DIR.glob('apps/*/migrations/*.py'))
     files_to_exclude = filter(lambda x: x.name != '__init__.py', files)
-
     for file in files_to_exclude:
         remove(file)
         print(f'File removed: {file.relative_to(BASE_DIR)}')
+
 
 if __name__ == '__main__':
     remove_migrations()
