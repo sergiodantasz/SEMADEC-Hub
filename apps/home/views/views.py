9ae6f6c0828739d7d958054325bd1b27c8a908f1
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-17 14:35:13 -0300

    fix: fix missing import

--- apps/home/views/views.py
+++ apps/home/views/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from apps.editions.models import Edition
