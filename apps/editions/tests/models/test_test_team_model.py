eb637de6c3f4637d5d784020cb09c2a1cb7a0142
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-22 13:27:58 -0300

    test: improve tests for `TestTeam` model

--- apps/editions/tests/models/test_test_team_model.py
+++ apps/editions/tests/models/test_test_team_model.py
@@ -1,9 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from pytest import mark
 from pytest import raises as assert_raises
 
 
-@mark.skip
-def test_test_team_model_score_cannot_be_negative(db, test_team_fixture):
-    with assert_raises(IntegrityError):
-        reg = test_team_fixture(score=-10)
+def test_test_team_model_score_can_be_null(db, test_team_fixture):
+    reg = test_team_fixture(score=None)
+    assert reg.score is None
+
+
+def test_test_team_model_dunder_str_method_returns_team_name(db, test_team_fixture):
+    reg = test_team_fixture()
+    assert str(reg) == reg.team.name
