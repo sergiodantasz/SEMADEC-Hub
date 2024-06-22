e32b2d61bb8daea3f20b2c4762c9fdcf9d1bab0c
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-22 15:55:48 -0300

    test: improve tests for competitions views

--- apps/competitions/tests/views/test_competitions_views.py
+++ apps/competitions/tests/views/test_competitions_views.py
@@ -1,3 +1,4 @@
+from django.test import RequestFactory
 from django.urls import resolve, reverse
 
 from apps.competitions import views
@@ -6,3 +7,9 @@ from apps.competitions import views
 def test_competitions_viewname_redirects_to_sports_view():
     view = resolve(reverse('competitions:home'))
     assert view.func.view_class is views.CompetitionView
+
+
+def test_competitions_view_redirects_to_sports_view():
+    request = RequestFactory().get('/competitions/')
+    response = views.CompetitionView.as_view()(request)
+    assert response.url == reverse('competitions:sports:home')
