643349dcddfe14ca3e5b306926d34292736520c0
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-23 10:41:25 -0300

    test: update tests for course views

--- apps/teams/tests/views/test_course_views.py
+++ apps/teams/tests/views/test_course_views.py
@@ -8,3 +8,11 @@ from apps.teams.views import views_courses as views
 def test_course_list_view_get_queryset_method_returns_queryset():
     view = views.CourseListView()
     assert isinstance(view.get_queryset(), QuerySet)
+
+
+def test_course_search_view_get_queryset_method_returns_queryset(db):
+    request = RequestFactory().get(reverse('teams:courses:search'))
+    request.GET |= {'q': 'test'}
+    view = views.CourseSearchView()
+    view.request = request
+    assert isinstance(view.get_queryset(), QuerySet)
