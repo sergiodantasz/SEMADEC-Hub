4dd9849f14c29b1189899f0d979527d77b0366e5
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-25 13:46:38 -0300

    test: add tests

--- apps/teams/tests/views/test_class_views.py
+++ apps/teams/tests/views/test_class_views.py
@@ -1,8 +1,12 @@
+from django.contrib.messages import get_messages
+from django.contrib.messages.storage.fallback import FallbackStorage
 from django.db.models.query import QuerySet
-from django.test import RequestFactory
+from django.test import Client, RequestFactory
 from django.urls import resolve, reverse
 
+from apps.teams.models import Course
 from apps.teams.views import views_classes as views
+from apps.users.tests.factories import UserFactory
 
 
 def test_class_list_view_get_queryset_method_returns_queryset():
@@ -15,3 +19,26 @@ def test_class_list_view_context_data_is_dict(db):
     request.resolver_match = resolve(reverse('teams:classes:home'))
     response = views.ClassListView.as_view()(request)
     assert isinstance(response.context_data, dict)
+
+
+def test_class_search_view_get_queryset_method_returns_queryset(db):
+    request = RequestFactory().get(reverse('teams:classes:search'))
+    request.GET |= {'q': 'test'}
+    view = views.ClassSearchView()
+    view.request = request
+    assert isinstance(view.get_queryset(), QuerySet)
+
+
+def test_class_search_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('teams:classes:search'))
+    request.GET |= {'q': 'test'}
+    response = views.ClassSearchView.as_view()(request)
+    assert isinstance(response.context_data, dict)
+
+
+def test_class_create_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('teams:classes:create'))
+    view = views.ClassCreateView()
+    view.setup(request)
+    context = view.get_context_data()
+    assert isinstance(context, dict)
--- apps/teams/tests/views/test_course_views.py
+++ apps/teams/tests/views/test_course_views.py
@@ -3,7 +3,10 @@ from django.test import RequestFactory
 from django.urls import resolve, reverse
 from pytest import mark
 
+from apps.teams.models import Course
+from apps.teams.tests.factories import CourseFactory
 from apps.teams.views import views_courses as views
+from apps.users.tests.factories import UserFactory
 
 
 def test_course_list_view_get_queryset_method_returns_queryset():
@@ -30,4 +33,27 @@ def test_course_search_view_context_data_is_dict(db):
     request = RequestFactory().get(reverse('teams:courses:search'))
     request.GET |= {'q': 'test'}
     response = views.CourseSearchView.as_view()(request)
+    assert isinstance(response.context_data, dict)
+
+
+def test_course_create_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('teams:courses:create'))
+    request.resolver_match = resolve(reverse('teams:courses:create'))
+    request.user = UserFactory(is_admin=True)
+    response = views.CourseCreateView.as_view()(request)
+    assert isinstance(response.context_data, dict)
+
+
+@mark.skip
+def test_course_edit_view_context_data_is_dict(db):
+    CourseFactory(slug='test-slug')
+    request = RequestFactory().post(
+        reverse('teams:courses:edit', kwargs={'slug': 'test-slug'})
+    )
+    request.resolver_match = resolve(
+        reverse('teams:courses:edit', kwargs={'slug': 'test-slug'})
+    )
+    request.user = UserFactory(is_admin=True)
+    # request.course = Course.objects.get(slug='test-slug')
+    response = views.CourseEditView.as_view()(request)
     assert isinstance(response.context_data, dict)
