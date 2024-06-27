a3fd779fd78839fb1412c794eadc43faa29395b8
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-27 10:00:31 -0300

    test: add tests

--- apps/handlers/tests/views/test_handlers_views.py
+++ apps/handlers/tests/views/test_handlers_views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import BadRequest, PermissionDenied
 from django.http import Http404
 from django.test import RequestFactory
 from django.urls import resolve
@@ -6,8 +7,25 @@ from pytest import mark
 from apps.handlers import views
 
 
-@mark.skip
 def test_handler_view_page_not_found_404_loads_correct_view():
     request = RequestFactory().get('/')
     view = views.page_not_found_404(request, Http404)
-    ...
+    assert view.status_code == 404
+
+
+def test_handler_view_page_server_error_500_loads_correct_view():
+    request = RequestFactory().get('/')
+    view = views.server_error_500(request)
+    assert view.status_code == 500
+
+
+def test_handler_view_page_permission_denied_403_loads_correct_view():
+    request = RequestFactory().get('/')
+    view = views.permission_denied_403(request, PermissionDenied)
+    assert view.status_code == 403
+
+
+def test_handler_view_page_bad_request_400_loads_correct_view():
+    request = RequestFactory().get('/')
+    view = views.bad_request_400(request, BadRequest)
+    assert view.status_code == 400
