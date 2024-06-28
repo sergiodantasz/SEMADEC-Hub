84f5c1368767ec314c697a665ade7e37355c4f17
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-28 15:04:19 -0300

    test: add tests

--- apps/editions/tests/views/test_editions_views.py
+++ apps/editions/tests/views/test_editions_views.py
@@ -1,5 +1,9 @@
+import django
+from django.conf import settings
+from django.contrib.messages import get_messages
+from django.contrib.messages.storage.fallback import FallbackStorage
 from django.db.models.query import QuerySet
-from django.test import RequestFactory
+from django.test import Client, RequestFactory
 from django.urls import resolve, reverse
 
 from apps.competitions.tests.factories import SportFactory
@@ -7,6 +11,8 @@ from apps.editions import views
 from apps.editions.forms import EditionForm, EditionTeamForm
 from apps.teams.tests.factories import TeamFactory
 from apps.users.tests.factories import UserFactory
+
+django.setup()
 
 
 def test_edition_list_view_get_queryset_method_returns_queryset():
@@ -32,7 +38,7 @@ def test_edition_detail_view_context_data_is_dict(db, edition_fixture):
 
 
 def test_edition_search_view_get_queryset_method_returns_queryset(db):
-    request = RequestFactory().get(reverse('editions:home'))
+    request = RequestFactory().get(reverse('editions:search'))
     request.GET |= {'q': 'test'}
     view = views.EditionSearchView()
     view.setup(request)
@@ -41,7 +47,7 @@ def test_edition_search_view_get_queryset_method_returns_queryset(db):
 
 
 def test_edition_search_view_context_data_is_dict(db):
-    request = RequestFactory().get(reverse('editions:home'))
+    request = RequestFactory().get(reverse('editions:search'))
     request.GET |= {'q': 'test'}
     response = views.EditionSearchView.as_view()(request)
     assert isinstance(response.context_data, dict)
@@ -50,11 +56,44 @@ def test_edition_search_view_context_data_is_dict(db):
 def test_edition_create_view_context_data_is_dict(db):
     SportFactory()
     TeamFactory()
-    request = RequestFactory().get(reverse('editions:home'))
-    request.resolver_match = resolve(reverse('editions:home'))
+    request = RequestFactory().get(reverse('editions:create'))
+    request.resolver_match = resolve(reverse('editions:create'))
     request.user = UserFactory(is_admin=True)
     response = views.EditionCreateView.as_view()(request)
     assert isinstance(response.context_data, dict)
+
+
+def test_edition_create_view_get_raises_error_if_no_team(db):
+    SportFactory()
+    c = Client()
+    request = c.get(reverse('editions:create')).wsgi_request
+    view = views.EditionCreateView()
+    view.setup(request)
+    view.get(request)
+    messages = list(get_messages(request))
+    assert len(messages) == 1
+
+
+def test_edition_create_view_get_raises_error_if_no_sport(db):
+    TeamFactory()
+    c = Client()
+    request = c.get(reverse('editions:create')).wsgi_request
+    view = views.EditionCreateView()
+    view.setup(request)
+    view.get(request)
+    messages = list(get_messages(request))
+    assert len(messages) == 1
+
+
+def test_edition_create_view_get_renders_response_if_no_errors(db):
+    SportFactory()
+    TeamFactory()
+    c = Client()
+    request = c.get(reverse('editions:create')).wsgi_request
+    view = views.EditionCreateView()
+    view.setup(request)
+    response = view.get(request)
+    assert response._request.path == reverse('editions:create')
 
 
 def test_edition_edit_view_context_data_is_dict(db, edition_fixture):
@@ -85,3 +124,4 @@ def test_edition_edit_view_get_form_teams_returns_form(db, edition_fixture):
     view.object = obj
     form = view.get_form_teams()
     assert isinstance(form.empty_form, EditionTeamForm)
+
---
+++ suap/tests/test_backends.py
@@ -0,0 +1,55 @@
+import pytest
+
+from apps.users.tests.factories import UserFactory
+from suap.backends import SuapOAuth2, UserData
+
+
+@pytest.fixture
+def user_data_fixture():
+    data = {
+        'identificacao': '281718921',
+        'nome_registro': 'nome',
+        'cpf': 'cpf',
+        'email_secundario': 'email1@gmail.com',
+        'email_google_classroom': 'email2@gmail.com',
+        'email_academico': 'email3@gmail.com',
+        'tipo_usuario': 'tipo',
+        'sexo': 'sexo',
+        'data_de_nascimento': 'data',
+        'foto': 'foto.png',
+    }
+    return data
+
+
+def test_userdata_class_set_user_permissions_update_reg_permissions(
+    db, user_data_fixture
+):
+    data = user_data_fixture
+    UserFactory(
+        registration=data.get('identificacao'),
+        is_admin=True,
+        is_staff=True,
+        is_superuser=True,
+    )
+    user_data = UserData(data)
+    assert all(
+        (
+            user_data.is_admin is True,
+            user_data.is_staff is True,
+            user_data.is_superuser is True,
+        )
+    )
+
+
+@pytest.mark.skip
+def test_suapoauth2_user_data_returns_response():
+    obj = SuapOAuth2()
+    response = obj.user_data(access_token='test_token')
+    ...
+
+
+def test_suapoauth2_get_user_details_returns_dict(db, user_data_fixture):
+    obj = SuapOAuth2()
+    data = user_data_fixture
+    response = obj.get_user_details(data)
+    assert isinstance(response, dict)
