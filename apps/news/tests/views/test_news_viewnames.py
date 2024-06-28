9e60c959286aa64c1e92047242e1e3b6233198b1
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-06-28 12:59:48 -0300

    test: add tests

---
+++ apps/news/tests/views/test_news_viewnames.py
@@ -0,0 +1,33 @@
+from django.urls import resolve, reverse
+
+from apps.news import views
+
+
+def test_news_list_viewname_redirects_to_news_list_view():
+    view = resolve(reverse('news:home'))
+    assert view.func.view_class is views.NewsListView
+
+
+def test_news_search_viewname_redirects_to_news_search_view():
+    view = resolve(reverse('news:search'))
+    assert view.func.view_class is views.NewsSearchView
+
+
+def test_news_create_viewname_redirects_to_create_view():
+    view = resolve(reverse('news:create'))
+    assert view.func.view_class is views.NewsCreateView
+
+
+def test_delete_viewname_redirects_to_delete_view():
+    view = resolve(reverse('news:delete', kwargs={'slug': 'test-slug'}))
+    assert view.func.view_class is views.NewsDeleteView
+
+
+def test_edit_viewname_redirects_to_edit_view():
+    view = resolve(reverse('news:edit', kwargs={'slug': 'test-slug'}))
+    assert view.func.view_class is views.NewsEditView
+
+
+def test_view_viewname_redirects_to_view_view():
+    view = resolve(reverse('news:view', kwargs={'slug': 'slug-test'}))
+    assert view.func.view_class is views.NewsDetailView
--- apps/news/tests/views/test_news_views.py
+++ apps/news/tests/views/test_news_views.py
@@ -1,33 +1,78 @@
+from django.db.models.query import QuerySet
+from django.test import RequestFactory
 from django.urls import resolve, reverse
+from pytest import mark
 
+from apps.competitions.forms import SportForm
+from apps.home.tests.factories import TagFactory
 from apps.news import views
+from apps.users.tests.factories import UserFactory
 
 
-def test_news_list_viewname_redirects_to_news_list_view():
-    view = resolve(reverse('news:home'))
-    assert view.func.view_class is views.NewsListView
+def test_news_list_view_get_queryset_method_returns_queryset():
+    view = views.NewsListView()
+    assert isinstance(view.get_queryset(), QuerySet)
 
 
-def test_news_search_viewname_redirects_to_news_search_view():
-    view = resolve(reverse('news:search'))
-    assert view.func.view_class is views.NewsSearchView
+def test_news_list_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('news:home'))
+    request.resolver_match = resolve(reverse('news:home'))
+    response = views.NewsListView.as_view()(request)
+    assert isinstance(response.context_data, dict)
 
 
-def test_news_create_viewname_redirects_to_create_view():
-    view = resolve(reverse('news:create'))
-    assert view.func.view_class is views.NewsCreateView
+def test_news_search_view_get_queryset_method_returns_queryset(db):
+    request = RequestFactory().get(reverse('news:search'))
+    request.GET |= {'q': 'test'}
+    view = views.NewsSearchView()
+    view.setup(request)
+    queryset = view.get_queryset()
+    assert isinstance(queryset, QuerySet)
 
 
-def test_delete_viewname_redirects_to_delete_view():
-    view = resolve(reverse('news:delete', kwargs={'slug': 'test-slug'}))
-    assert view.func.view_class is views.NewsDeleteView
+def test_news_search_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('news:search'))
+    request.GET |= {'q': 'test'}
+    response = views.NewsSearchView.as_view()(request)
+    assert isinstance(response.context_data, dict)
 
 
-def test_edit_viewname_redirects_to_edit_view():
-    view = resolve(reverse('news:edit', kwargs={'slug': 'test-slug'}))
-    assert view.func.view_class is views.NewsEditView
+def test_news_search_view_get_tags_from_url_return_tags(db):
+    request = RequestFactory().get(reverse('news:search'))
+    request.GET |= {'q': 'test', 'tags': ('tag_test',)}
+    view = views.NewsSearchView()
+    view.setup(request)
+    response = view.get_tags_from_url()
+    assert len(response) > 0
 
 
-def test_view_viewname_redirects_to_view_view():
-    view = resolve(reverse('news:view', kwargs={'slug': 'slug-test'}))
-    assert view.func.view_class is views.NewsDetailView
+def test_news_search_view_get_tags_return_tags(db):
+    TagFactory(slug='tag1')
+    TagFactory(slug='tag2')
+    request = RequestFactory().get(reverse('news:search'))
+    request.GET |= {'q': 'test'}
+    view = views.NewsSearchView()
+    view.setup(request)
+    response = view.get_tags(['tag1', 'tag2', '', 'tag2'])
+    assert len(response) == 2
+
+
+def test_news_create_view_context_data_is_dict(db):
+    request = RequestFactory().get(reverse('news:create'))
+    request.resolver_match = resolve(reverse('news:create'))
+    request.user = UserFactory(is_admin=True)
+    response = views.NewsCreateView.as_view()(request)
+    assert isinstance(response.context_data, dict)
+
+
+@mark.skip
+def test_news_create_view(db, news_form_fixture):
+    form = news_form_fixture()
+    request = RequestFactory().get(reverse('news:create'))
+    request.resolver_match = resolve(reverse('news:create'))
+    request.user = UserFactory(is_admin=True)
+    request._messages = {}
+    view = views.NewsCreateView()
+    view.setup(request)
+    response = view.form_valid(form)
+    assert isinstance(response.context_data, dict)
