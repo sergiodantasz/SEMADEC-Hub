b3ae5d05c7a4baa0d687040c668f9901b258c14a
Sérgio Dantas <sergiodnts828@gmail.com>
2024-06-28 10:11:20 -0300

    test: fix document view tests

--- apps/documents/tests/views/test_documents_views.py
+++ apps/documents/tests/views/test_documents_views.py
@@ -5,9 +5,9 @@ from apps.documents import views
 
 def test_documents_viewname_redirects_to_documents_view():
     view = resolve(reverse('documents:home'))
-    assert view.func is views.documents_collection
+    assert view.func.view_class is views.DocumentListView
 
 
 def test_documents_search_viewname_redirects_to_documents_search_view():
     view = resolve(reverse('documents:search'))
-    assert view.func is views.search_document_collection
+    assert view.func.view_class is views.DocumentSearchView
