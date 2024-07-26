b311add9469a87fbc3affa21c57f37d60dc7c956
Joamerson Islan <joamersonislan38@gmail.com>
2024-07-26 13:13:44 -0300

    chore: update docstrings of `custom_tags.py`

--- apps/home/templatetags/custom_tags.py
+++ apps/home/templatetags/custom_tags.py
@@ -1,10 +1,14 @@
 from os import path
+from typing import Any
 
 from django import template
 from django.db.models import QuerySet
+from django.forms.boundfield import BoundField
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.safestring import mark_safe
+
+from apps.users.models import User
 
 register = template.Library()
 
@@ -44,7 +48,13 @@ def load_regs(
 
 
 @register.simple_tag()
-def check_error(field):
+def check_error(field: BoundField):
+    """Check errors inside a form field
+    Args:
+        field (BoundField): BoundField object that represents the form field
+    Returns:
+        SafeString | str: Empty string or containing HTML error containers
+    """
     if field.errors:
         container_outer = '<div class="field-errors">{}</div>'
         errors_list = []
@@ -58,7 +68,19 @@ def check_error(field):
 
 
 @register.inclusion_tag('global/partials/_create-form-button.html')
-def load_create_button(user, namespace, label, dispatcher=None,id_field='pk'):
+def load_create_button(
+    user: User, namespace: str, label: str, dispatcher: Any = None, id_field: str = 'pk'
+):
+    """Controls the loading of create button inside HTML pages
+    Args:
+        user (User): User object of currently logged user
+        namespace (str): Namespace that points to a URL
+        label (str): Label to be placed inside button element
+        dispatcher (Any, optional): The dispatcher value to be appended to provided namespace
+        id_field (str, optional): The dispatcher field to be appended to provided namespace
+    Returns:
+        dict: Dict object to be sent to form button partial
+    """
     if dispatcher:
         return {
             'user': user,
@@ -70,4 +92,6 @@ def load_create_button(user, namespace, label, dispatcher=None,id_field='pk'):
 
 @register.filter
 def filename(value):
+    """Returns
+    """
     return path.basename(value.file.name)
