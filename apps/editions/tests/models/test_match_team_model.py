0fc3c6bfd37685b5786cdb3a4b41431fd2376402
Joamerson Islan <joamersonislan38@gmail.com>
2024-08-04 15:58:15 -0300

    test: update tests

--- apps/editions/tests/models/test_match_team_model.py
+++ apps/editions/tests/models/test_match_team_model.py
@@ -4,10 +4,10 @@ from pytest import mark
 from pytest import raises as assert_raises
 
 
-@mark.skip
 def test_match_team_model_score_cannot_be_negative(db, match_team_fixture):
-    with assert_raises(IntegrityError):
-        reg = match_team_fixture(score=-10)
+    reg = match_team_fixture(score=-10)
+    with assert_raises(ValidationError):
+        reg.full_clean()
 
 
 def test_match_team_model_dunder_str_method_returns_team_name(db, match_team_fixture):
